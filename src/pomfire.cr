10188a1976f14313ac99baaf1bcbc4210a9b622d
RX14 <chris@rx14.co.uk>
2017-08-01 00:04:10 +0100

    Eat ECONNRESET too

--- src/pomfire.cr
+++ src/pomfire.cr
@@ -3,10 +3,11 @@ require "./b2"
 require "./pomfire/*"
 
 module Pomfire
-  def self.eat_epipe
+  def self.eat_client_errors
     yield
   rescue errno : Errno
-    raise errno unless errno.errno == Errno::EPIPE
+    raise errno unless errno.errno == Errno::EPIPE ||
+                       errno.errno == Errno::ECONNRESET
   end
 
   def self.handle_request(ctx, file_cache)
@@ -14,7 +15,7 @@ module Pomfire
 
     if file_name == ""
       # Root
-      eat_epipe do
+      eat_client_errors do
         ctx.response.puts "aww.moe is in readonly mode, uploads coming soon!"
         ctx.response.flush
       end
@@ -35,24 +36,24 @@ module Pomfire
 
       puts "Serving: #{file_name} #{mime_type} #{res}"
 
-      eat_epipe do
+      eat_client_errors do
         IO.copy(io, ctx.response)
       end
     end
 
     if res.missing?
       ctx.response.status_code = 404
-      eat_epipe do
+      eat_client_errors do
         ctx.response.puts "Not Found"
       end
     end
 
-    eat_epipe do
+    eat_client_errors do
       ctx.response.flush
     end
   rescue ex
     ctx.response.status_code = 500
-    eat_epipe do
+    eat_client_errors do
       ctx.response.puts "#{ex.message} (#{ex.class})"
       ctx.response.flush
     end
